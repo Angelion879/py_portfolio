0c29c1f89e6dfd048d6f714048ee4c1da71972a6
Angelion879 <diangelis879@outlook.com>
2025-01-26 11:31:11 -0300

    (update) language switch url

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.conf.urls.i18n import i18n_patterns
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+
+    # Language switch path
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
