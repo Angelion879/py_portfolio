b5ad3db61a1a52c2b59d574b982692dcf99c0426
Angelion879 <diangelis879@outlook.com>
2025-01-26 12:26:55 -0300

    (feat) set up necessary views and urls

--- base/urls.py
+++ base/urls.py
@@ -4,7 +4,18 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('feed/', views.feed, name="feed"),
+    path('post/<slug:slug>/', views.post, name="post"),
+    path('contact/', views.contact, name="contact"),
 
     # Language switch path
     path('i18n/', include('django.conf.urls.i18n')),
+
+    # CRUD paths
+    path('create_post/', views.create_post, name="create_post"),
+    path('update_post/<slug:slug>/', views.update_post, name="update_post"),
+    path('delete_post/<slug:slug>/', views.delete_post, name="delete_post"),
+
+    # Send Email Path
+    path('send_email/', views.send_email, name='send_email')
 ]
