6c27062aba3c4d2a379a9853bb38db4beb56bd1e
Angelion879 <diangelis879@outlook.com>
2025-01-27 12:49:53 -0300

    (update) postgreDB settings and migration

--- biangelis/settings.py
+++ biangelis/settings.py
@@ -94,8 +94,11 @@ WSGI_APPLICATION = 'biangelis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('BD_NAME'),
+        'HOST': env('BD_ADDRESS'),
+        'USER': env('BD_USERNAME'),
+        'PASSWORD': env('BD_PASSWORD')
     }
 }
 
