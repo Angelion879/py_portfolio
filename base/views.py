15a6571644b8d6c5c9b9e0d7cc15f9aef7234775
Angelion879 <diangelis879@outlook.com>
2025-01-27 10:34:45 -0300

    (update) homepage icons

--- base/views.py
+++ base/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from .generator import home_repeat
+
 # Create your views here.
 def home_page(request):
-    return render(request, 'pages/home.html')
+    repeat = home_repeat()
+
+    context = {'rep':repeat}
+    return render(request, 'pages/home.html', context)
