8cf373d5ae427108ed10ee71bffdc9c2fb9c3b8f
Angelion879 <diangelis879@outlook.com>
2025-01-30 15:36:15 -0300

    (test-update) connect to postgre db

--- biangelis/settings.py
+++ biangelis/settings.py
@@ -93,11 +93,12 @@ WSGI_APPLICATION = 'biangelis.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db(),
 }
+
+DATABASES["default"]["HOST"] = env('BD_ADDRESS')
+DATABASES["default"]["PORT"] = 5432
+#REMEMBER TO TURN IT ON FIRST
 
 '''
     'default': {
