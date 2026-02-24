746ac80afd19464d357b6bfe4c17342ef155c62e
Angelion879 <diangelis879@outlook.com>
2026-02-24 18:29:33 -0300

    (update) homepage content

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -4,5 +4,7 @@ from django.utils.translation import gettext as _
 
 # Create your views here.
 def home(request):
-    context = None
+    repeat = lambda: list(range(1, 15))
+
+    context = {'repeat':repeat}
     return render(request, 'pages/home.html', context)
