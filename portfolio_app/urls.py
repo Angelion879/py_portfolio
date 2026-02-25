7efc713ca2f50fa73bd6337620ff2563e9af8ad7
Angelion879 <diangelis879@outlook.com>
2026-02-25 12:42:22 -0300

    (create) email sender feature

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("contact/", views.contact, name="contact"),
 
+    # Send Email Path
+    path('send_email/', views.send_email, name='send_email'),
+
     # Language path
     path('i18n/', include('django.conf.urls.i18n')),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
 from django.utils.translation import gettext as _
+
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
+
+from biangelis.keys import receiver
+
 
 # Create your views here.
 def home(request):
@@ -10,9 +16,31 @@ def home(request):
     return render(request, 'pages/home.html', context)
 
 def contact(request):
-    repeat = lambda: list(range(1,151))
+    wlpp_repeat = lambda: list(range(1,151))
 
-    context = {'repeat':repeat}
+    context = {'repeat':wlpp_repeat}
     return render(request, 'pages/contact.html', context)
 
 # Email sending feat
+def send_email(request):
+    wlpp_repeat = lambda: list(range(1,151))
+
+    if request.method == 'POST':
+        template = render_to_string('email_structure.html',{
+            'name':request.POST['name'],
+            'email':request.POST['email'],
+            'message':request.POST['message'],
+        })
+
+        email = EmailMessage(
+            subject=request.POST['subject'],
+            body=template,
+            from_email=settings.EMAIL_HOST_USER,
+            to=receiver
+        )
+
+        email.fail_silently=False
+        email.send()
+
+    context = {'repeat':wlpp_repeat}
+    return render(request, 'pages/confirmation.html', context)
