2221c54725d84a5acd2ec2fe15b9127ec427a06c
Angelion879 <diangelis879@outlook.com>
2026-02-25 12:04:33 -0300

    (add) view and paths to contact page

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("contact/", views.contact, name="contact"),
 
     # Language path
     path('i18n/', include('django.conf.urls.i18n')),
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -8,3 +8,11 @@ def home(request):
 
     context = {'repeat':repeat}
     return render(request, 'pages/home.html', context)
+
+def contact(request):
+    repeat = lambda: list(range(1,151))
+
+    context = {'repeat':repeat}
+    return render(request, 'pages/contact.html', context)
+
+# Email sending feat
