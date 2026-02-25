79f58977567722c123320d2d6406dd3aedc8e350
Angelion879 <diangelis879@outlook.com>
2026-02-24 23:43:40 -0300

    (add) i18n translation url

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+
+    # Language path
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
