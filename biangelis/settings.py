63d93656b6a8bcf429e20dd5bea45358d88b6c2e
Angelion879 <diangelis879@outlook.com>
2026-03-04 17:55:07 -0300

    (update) static files dirs

--- biangelis/settings.py
+++ biangelis/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'portfolio_app/static')
 ]
 
 MEDIA_URL = '/images/'
